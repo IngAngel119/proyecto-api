73e2c4b8fc1470f8ab18bc4fb41d2953f461875f
IngAngel119 <aescalantegarza@gmail.com>
2025-05-07 17:22:19 -0600

    Validator

--- app/Http/Controllers/WordController.php
+++ app/Http/Controllers/WordController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Word;
 use App\Models\UserResponse;
-use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Validator;
 
 class WordController extends Controller
 {
