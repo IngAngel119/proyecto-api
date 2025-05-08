84aeb556c1a1494061435021e5503667c12f3f0a
IngAngel119 <aescalantegarza@gmail.com>
2025-05-08 12:57:30 -0600

    ruta api

--- routes/api.php
+++ routes/api.php
@@ -18,5 +18,6 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/categories', [CategoryController::class, 'index']);
     Route::get('/daily-word', [WordController::class, 'dailyWord']);
+    Route::get('/search', [WordController::class, 'searchWords']);
     Route::post('/answer', [UserResponseController::class, 'store']);
 });
