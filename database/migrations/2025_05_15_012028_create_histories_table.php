b5316c89a528703fcf10afd3cb89625720379ecd
IngAngel119 <aescalantegarza@gmail.com>
2025-05-14 19:37:21 -0600

    null error

--- database/migrations/2025_05_15_012028_create_histories_table.php
+++ database/migrations/2025_05_15_012028_create_histories_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('histories', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->foreignId('word_id')->constrained()->onDelete('cascade');
+            $table->foreignId('word_id')->nullable()->constrained()->onDelete('cascade');
             $table->string('event');
             $table->timestamps();
         });
