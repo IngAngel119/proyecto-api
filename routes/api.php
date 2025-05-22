b66b59193428b6b2372ceed827b03491741e02d8
IngAngel119 <aescalantegarza@gmail.com>
2025-05-22 09:24:49 -0600

    api logout

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,7 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/categories', [CategoryController::class, 'index']);
     Route::get('/daily-word', [WordController::class, 'dailyWord']);
     Route::get('/search', [WordController::class, 'searchWords']);
